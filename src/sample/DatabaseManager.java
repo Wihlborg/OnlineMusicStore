173de18295b8dd837426cddbfe7ed0e417d8560c
Henrik Wihlborg <henrik.vihlborg0017@stud.hkr.se>
2018-04-13 11:18:08 +0200

    Password SQL statement

--- src/sample/DatabaseManager.java
+++ src/sample/DatabaseManager.java
@@ -1,18 +1,34 @@
 package sample;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseManager {
+    private String url;
+    private Connection c;
+    private Statement st;
     public DatabaseManager() {
         try {
-            String url = "jdbc:mysql://den1.mysql2.gear.host:3306/onlinemusicstore?user=onlinemusicstore&password=OnlineMusicStore!";
-            Connection c = DriverManager.getConnection(url);
-            Statement st = c.createStatement();
+            url = "jdbc:mysql://den1.mysql2.gear.host:3306/onlinemusicstore?user=onlinemusicstore&password=OnlineMusicStore!";
+            c = DriverManager.getConnection(url);
+            st = c.createStatement();
         } catch (SQLException ex){
             ex.printStackTrace();
         }
+    }
+
+    public boolean passwordCheck(String username, String password){
+        try {
+            ResultSet rs = st.executeQuery("SELECT * FROM users WHERE username= '" + username +"';");
+            while (rs.next()){
+                if (rs.getString("password").equalsIgnoreCase(password)){
+                    return true;
+                }
+            }
+
+        } catch (SQLException ex){
+            ex.printStackTrace();
+
+        }
+        return false;
     }
 }
