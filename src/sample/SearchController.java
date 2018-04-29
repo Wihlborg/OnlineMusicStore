e3b455f520c9ff0665ef5ec195518f45f014847b
Oskar Forssman <Oskar.Forssman@hotmail.com>
2018-04-29 15:59:32 +0200

    bajs

--- src/sample/SearchController.java
+++ src/sample/SearchController.java
@@ -1,20 +1,31 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
+
 
 public class SearchController {
     private ArrayList<Song> songs = new ArrayList<>();
     private ArrayList<Album> albums = new ArrayList<>();
     private DatabaseManager dm = DatabaseManager.getInstance();
 
-    public void search(String search){
+
+@FXML  TextField searchtextField;
+@FXML  TextArea textsearchShow;
+
+
+
+public void search(String search){
         songs = dm.getSongs(search);
         albums = dm.getAlbums(search);
     }
@@ -42,5 +53,12 @@ public class SearchController {
             e.printStackTrace();
 
         }
+    }
+    @FXML
+    public void searchFunction(ActionEvent event){
+
+        String searchD = searchtextField.getText().toString();
+
+                search(searchD);
     }
 }
