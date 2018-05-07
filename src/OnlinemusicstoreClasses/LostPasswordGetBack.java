7b0b71ad2bff081a3b646ff606810237b99033c0
stina <stinaahlstroem@gmail.com>
2018-05-07 14:41:14 +0200

    ""

--- src/OnlinemusicstoreClasses/LostPasswordGetBack.java
+++ src/OnlinemusicstoreClasses/LostPasswordGetBack.java
@@ -6,12 +6,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class LostPasswordGetBack {
+
 
     @FXML
     void getBack(ActionEvent event) {
@@ -34,6 +36,25 @@ public class LostPasswordGetBack {
             e.printStackTrace();
 
         }
+
+    }
+
+        if (UserNameLostPassword.getText().isEmpty() || UserNameLostPassword.getText().isEmpty()) {
+        Alert emptyAlert = new Alert(Alert.AlertType.ERROR, "Field must not be empty");
+        emptyAlert.show();
+    } else {
+        String username = UserNameLostPassword.getText();
+        String password = emailLostPassword.getText();
+
+
+        LostPasswordScene. (username, password);
+        SignUpController c2 = new SignUpController();
+        c2.changeToLogin(event);
+    }
+
+}
+
+
 
 
 
