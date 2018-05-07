d71d7ae873a65772c6e34587b968b99cd07910e3
stina <stinaahlstroem@gmail.com>
2018-05-07 14:36:52 +0200

    ""

--- src/OnlinemusicstoreClasses/LostPasswordGetBack.java
+++ src/OnlinemusicstoreClasses/LostPasswordGetBack.java
@@ -2,13 +2,42 @@ package OnlinemusicstoreClasses;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class LostPasswordGetBack {
 
     @FXML
     void getBack(ActionEvent event) {
+
+        try {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../OnlinemusicstoreFxml/LostPasswordScene.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+
+        } catch (NullPointerException ne) {
+
+            ne.getSuppressed();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+
+
+
 
     }
 
