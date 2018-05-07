ee73672421ce6667e2ec09f8618286811cb6a70a
stina <stinaahlstroem@gmail.com>
2018-05-07 14:28:27 +0200

    ""

--- src/sample/LostPasswordScene.java
+++ src/sample/LostPasswordScene.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -15,16 +16,15 @@ import java.io.IOException;
 public class LostPasswordScene {
 
 
-        @FXML
-        private TextField UserNameLostPassword;
+    @FXML
+    private TextField UserNameLostPassword;
 
-        @FXML
-        private TextField emailLostPassword;
-        
+    @FXML
+    private TextField emailLostPassword;
+
 
     @FXML
     void OkLostpassword(ActionEvent event) {
-
 
 
         try {
@@ -37,7 +37,7 @@ public class LostPasswordScene {
             Scene scene = new Scene(root);
             stage.setScene(scene);
 
-        } catch (NullPointerException ne){
+        } catch (NullPointerException ne) {
 
             ne.getSuppressed();
 
@@ -47,6 +47,18 @@ public class LostPasswordScene {
 
         }
 
+        if (UserNameLostPassword.getText().isEmpty() || UserNameLostPassword.getText().isEmpty()) {
+            Alert emptyAlert = new Alert(Alert.AlertType.ERROR, "Field must not be empty");
+            emptyAlert.show();
+        } else {
+            String username = UserNameLostPassword.getText();
+            String password = emailLostPassword.getText();
+
+
+            LostPasswordScene. (username, password);
+            SignUpController c2 = new SignUpController();
+            c2.changeToLogin(event);
+        }
 
     }
 
