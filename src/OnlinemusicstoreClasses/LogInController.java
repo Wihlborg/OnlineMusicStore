eb6add66ee39c5d67f7c1806568ce311ce093c20
Oskar Forssman <Oskar.Forssman@hotmail.com>
2018-05-09 12:16:59 +0200

    update password

--- src/OnlinemusicstoreClasses/LogInController.java
+++ src/OnlinemusicstoreClasses/LogInController.java
@@ -83,81 +83,10 @@ public void changeToMainMenu(ActionEvent event){
             e.printStackTrace();
         }
 }
-@FXML
-public void lostPassword(ActionEvent event,String username,String email,String sequrityQ){
 
-    boolean userName=db.checkUsername(username);
-    boolean seq=false;
 
-        if (userName==true &&seq==true ){
-        event.getSource();
-        String tooMail=email;
-        String fromMail="onlinemusicstorev1@gmail.com";
-        String password ="9x828x5w";
-        String subject="Lost Password";
-        String newPassword=UUID.randomUUID().toString();
-        //linka message så databasen uppdateras med nya passet.
-            db.changeUsersPassword(username,newPassword);
-        String host = "smtp.gmail.com";
-        Properties props = new Properties();
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host",host);
-        props.put("mail.smtp.user",fromMail);
-        props.put("mail.smtp.password",password);
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
-        Session session = Session.getInstance(props);
-        try {
-            Message message1 = new MimeMessage(session);
-            message1.setFrom(new InternetAddress(fromMail));
-            message1.setRecipient(Message.RecipientType.TO,new InternetAddress(tooMail));
-            message1.setSubject(subject);
-            message1.setText("Dear Customer,"
-                    + "\n\n"+newPassword);
-            Transport transport =session.getTransport("smtp");
-            transport.connect(host,fromMail,password);
-            transport.sendMessage(message1,message1.getAllRecipients());
-            transport.close();
-            System.out.println("Done");
 
-        } catch (MessagingException e) {
 
-        }
-    }
-    else if (userName==false){
-        Alert wrongPasswordAlert = new Alert(Alert.AlertType.ERROR, "Username Dont Exist");
-    wrongPasswordAlert.show();
-    }
-        else if (seq==false){
-            Alert wrongPasswordAlert = new Alert(Alert.AlertType.ERROR, "Sequrity Question Dont Match");
-            wrongPasswordAlert.show();
-        }
-
-}
-@FXML
-    public void changeToLostPassword(javafx.event.ActionEvent event){
-        try {
-            Node node = (Node) event.getSource();
-            Stage stage = (Stage) node.getScene().getWindow();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../OnlinemusicstoreFxml/LostPasswordScene.fxml"));
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-
-        } catch (NullPointerException ne){
-
-            ne.getSuppressed();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        }
-    }
-    @FXML
-    public void helpButton(ActionEvent event){
-
-    }
 }
 
 
--- src/OnlinemusicstoreClasses/LostPasswordScene.java
+++ src/OnlinemusicstoreClasses/LostPasswordScene.java
@@ -11,10 +11,18 @@ import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
+import java.util.Properties;
+import java.util.UUID;
 
 public class LostPasswordScene {
-
+DatabaseManager db=DatabaseManager.getInstance();
 
     @FXML
     private TextField UserNameLostPassword;
@@ -57,6 +65,59 @@ public class LostPasswordScene {
 
 
 
+        }
+
+    }
+    @FXML
+    public void lostPassword(ActionEvent event,String username,String email,String sequrityQ){
+
+        boolean userName=db.checkUsername(username);
+        boolean seq=false;
+
+
+
+        if (userName==true &&seq==true ){
+            event.getSource();
+            String tooMail="tauriainenjimmie@gmail.com";
+            String fromMail="onlinemusicstorev1@gmail.com";
+            String password ="9x828x5w";
+            String subject="Lost Password";
+            String newPassword= UUID.randomUUID().toString();
+            //linka message så databasen uppdateras med nya passet.
+            db.changeUsersPassword(username,newPassword);
+            String host = "smtp.gmail.com";
+            Properties props = new Properties();
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host",host);
+            props.put("mail.smtp.user",fromMail);
+            props.put("mail.smtp.password",password);
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.auth", "true");
+            Session session = Session.getInstance(props);
+            try {
+                Message message1 = new MimeMessage(session);
+                message1.setFrom(new InternetAddress(fromMail));
+                message1.setRecipient(Message.RecipientType.TO,new InternetAddress(tooMail));
+                message1.setSubject(subject);
+                message1.setText("Dear Customer,"
+                        + "\n\n"+newPassword);
+                Transport transport =session.getTransport("smtp");
+                transport.connect(host,fromMail,password);
+                transport.sendMessage(message1,message1.getAllRecipients());
+                transport.close();
+                System.out.println("Done");
+
+            } catch (MessagingException e) {
+
+            }
+        }
+        else if (userName==false){
+            Alert wrongPasswordAlert = new Alert(Alert.AlertType.ERROR, "Username Dont Exist");
+            wrongPasswordAlert.show();
+        }
+        else if (seq==false){
+            Alert wrongPasswordAlert = new Alert(Alert.AlertType.ERROR, "Sequrity Question Dont Match");
+            wrongPasswordAlert.show();
         }
 
     }
